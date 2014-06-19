c6b7aa7fb4af7784a62059eaf601ce8976de3cf7
nife0222 <nife0820@gmail.com>
2014-06-18 21:40:34 -0700

    mas varas

--- Ejercicio 2 - Canciones/src/ejercicioCanciones/Ejercicio2Canciones.java
+++ Ejercicio 2 - Canciones/src/ejercicioCanciones/Ejercicio2Canciones.java
@@ -6,6 +6,13 @@
 
 package ejercicioCanciones;
 
+import java.io.File;
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+//import org.farng.mp3.MP3File;
+//import org.farng.mp3.TagException;
+
 /**
  *
  * @author Nicolas
@@ -15,8 +22,22 @@ public class Ejercicio2Canciones {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         // TODO code application logic here
+        try {
+            File sourceFile = new File("C:/Users/Nicolas/Desktop");
+            sourceFile.setReadable(true);
+            sourceFile.setWritable(true);
+            System.out.println(sourceFile.canRead());
+            
+            
+            
+            
+            
+            
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
     }
     
 }
